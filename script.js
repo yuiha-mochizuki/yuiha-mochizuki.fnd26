aa5a9d56618addeea8c7e7daed8cf74dba886974
yuiha-mochizuki <136659754+yuiha-mochizuki@users.noreply.github.com>
2024-04-18 16:45:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-const hiraganaInput = document.querySelector('#Hiragana-input');
+const hiraganaInput = document.querySelector('#hiragana-input');
 const numberOfNotes = document.querySelector('.number-of-notes');
+const forImages = document.querySelector('#for-images');
 
-
-
+// 拗音を数えて引く
 function countHiragana(){
-  let test =  hiraganaInput.value.length;
-  if(test > 0){
+  let wordCount =  hiraganaInput.value.length;
+  if(wordCount > 0){
     const regex = /[ぁぃぅぇぉゃゅょゎァィゥェォャュョヮ]/g;
     let result1 = hiraganaInput.value.match(regex);
     let result2
     if (result1 !== null){
       result2 = result1.length
     }else{
-      return numberOfNotes.textContent = test;
+      return numberOfNotes.textContent = wordCount;
     }
-    return numberOfNotes.textContent = test - result2;
+    return numberOfNotes.textContent = wordCount - result2;
   }else{
     return numberOfNotes.textContent = "0";
   }
 }
-hiraganaInput.addEventListener('input',countHiragana);
+
+hiraganaInput.addEventListener('input', countHiragana);
+
+// 入力した文字と画像用の文字を一致させる
+function matchForImages(){
+  if (hiraganaInput.value !== null){
+    let result = forImages.value = hiraganaInput.value;
+    return result;
+  } else{
+    return "";
+  }
+}
+
+hiraganaInput.addEventListener('input', matchForImages)
